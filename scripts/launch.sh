eda560480fef7176ae272b27a066c8cd673a95de
Arthur <arthur.jourdan@epitech.eu>
2023-05-18 09:11:47 +1000

    feat(env/local.env): script to launch locally uses env and launches mongo db docker

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-set -o allexport
-source ./env/maestro.env
-set +o allexport
-
 cmake -S . -B build
 
 make -C build
 
+if [ "$1" == "--docker" ] ; then
+    docker compose --env-file ./env/unit_tests.env --profile mongo up --build &
+    DOCKER_PID=$!
+fi
+
+set -o allexport
+source ./env/local.env
+set +o allexport
+
+./build/maestro
+
+if [ "$1" == "--docker" ] ; then
+    kill $DOCKER_PID ## todo launch docker in background from docker arguments and stop with docker command
+fi
