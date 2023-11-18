54b1f277992ea2aad7cc9cfca300ce681772d855
Vincent Andrieu <vincent.andrieu@epitech.eu>
2023-11-18 10:27:54 +0100

    WIP: Editing DownloadedStack

--- src/Schemas/Files/DownloadedStack/DownloadedStackSchema.cpp
+++ src/Schemas/Files/DownloadedStack/DownloadedStackSchema.cpp
@@ -15,53 +15,27 @@ FilesDownloadedStackSchema::FilesDownloadedStackSchema(const mongocxx::database 
 {
 }
 
-void FilesDownloadedStackSchema::pushFile(const string &fileId, const Date &expirationDate, const string &content)
+void FilesDownloadedStackSchema::add(const string &fileId, const Date &expirationDate)
 {
-    /*todo*/
     MongoCXX::Document file_expiration =
         makeDocument(makeField("fileId", fileId), makeField("expirationDate", expirationDate.toBSON()));
 
     this->_model.insert_one(file_expiration.view());
 }
 
-NODISCARD string FilesDownloadedStackSchema::downloadFile(const string &fileId)
+NODISCARD std::vector<string> FilesDownloadedStackSchema::deleteExpiredFiles(const Date &expirationDate)
 {
-    /*todo*/
-
-    //    const std::ostringstream oss("");
-    //    std::ostream ostream(oss.rdbuf());
-    //    mongocxx::cursor cursor = this->_fileBucket.find(makeDocument(makeField("filename", fileId)).view());
-
-    //    this->_fileBucket.download_to_stream((*cursor.begin())["_id"].get_value(), &ostream);
-    //    return oss.str();
-
-    // todo return expiration date ?
-}
-
-/**
- * @brief Deletes expired files from mongoDB
- * @tparam StrIterator Any type of iterator over a string
- * @param expirationDate Date of expiry
- * @return First and last string iterator containing fileIds that have been deleted from DB and have to be deleted from filesystem
- */
-// template <typename StrIterator>
-//     requires std::input_iterator<StrIterator> && std::same_as<typename std::iterator_traits<StrIterator>::value_type, string>
-//  todo
-NODISCARD /* todo std::tuple<StrIterator, StrIterator>*/ std::vector<string>
-FilesDownloadedStackSchema::deleteExpiredFiles(const Date &expirationDate)
-{
-    /*todo*/
     const bsoncxx::document::value filter =
-        makeDocument(makeField("metadata.expirationDate", makeDocument(makeField("$lt", expirationDate.toBSON()))));
+        makeDocument(makeField("expirationDate", makeDocument(makeField("$lt", expirationDate.toBSON()))));
 
-    mongocxx::cursor cursor = this->_model.find(filter.view()); // todo
+    mongocxx::cursor cursor = this->_model.find(filter.view());
     std::vector<string> fileIds{};
 
     for (const auto &file : cursor) {
         this->_events.emit<const string &, const Date &>(
             Event::DOWNLOADEDSTACK_FILE_EXPIRATION,
-            file["filename"].get_string().value.to_string(),
-            Date(file["metadata"]["expirationDate"].get_date())
+            file["fileId"].get_string().value.to_string(),
+            Date(file["expirationDate"].get_date())
         );
         fileIds.push_back(file["_id"].get_oid().value.to_string());
     }
